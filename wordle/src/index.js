d93146094a7bcf500933554548efc6fd3877bfc7
gyumin_han <rbals0445@naver.com>
2023-03-05 01:43:34 +0900

    feat: curRow,curCol 제거, prev, current 따로 그려주는 아이디어, 다시 만들지 않고 Node를 넘겨주는 방식으로 해결 firstChild

--- wordle/src/index.js
+++ wordle/src/index.js
@@ -1,8 +1,6 @@
 const ROW = 6;
 const COL = 5;
-/**
- *  Make Wordle Grid (6 x 5)
- */
+
 const wordList = [
     'patio',
     'apple',
@@ -18,10 +16,21 @@ let randomIndex;
 let randomAnswer;
 let attempts = []; // 시도할때마다 숫자가 참.
 let currentAttempt = '';
-let curRow = 0, curCol = 0;
 
 const gridWrapper = document.getElementById('grid');
-// 첫번째 줄이 다차면 더이상 써지지 않는다. 윗줄부터..
+
+document.addEventListener('keyup',(e) => {
+    const char = onClickGridCell(e);
+    if(!char) return; 
+
+    updateGrid();
+});
+
+/**
+ *  Make Wordle Grid (6 x 5)
+ */
+initGrid();
+
 
 function initGlobalValue(){
     // 0~7 까지 나오게 하려면? 0~1까지 나오는데? 곱하기 10을 하고 8로 나눈 나머지. or 랜덤 * 길이 하고 floor 
@@ -29,21 +38,20 @@ function initGlobalValue(){
     // 1 * 30 => 27.xxx
     randomIndex =  ~~(Math.random() * wordList.length);
     randomAnswer = wordList[randomIndex];
-    curRow = 0;
-    curCol = 0;
     attempts = [];
     currentAttempt = '';
 }
 
-function getBgColor(row,col){
-    const curCharacter = attempts[row][col];
+function getBgColor(letters,i){
+    const attemptLetter = letters[i];
+    const correctLetter = randomAnswer[i];
 
-    if(curCharacter === randomAnswer[col]){
+    if(attemptLetter === correctLetter){
         return 'green';
     }
 
-    if(randomAnswer.includes(curCharacter)){
-        return 'yellow';
+    if(randomAnswer.includes(attemptLetter)){
+        return '#aeae2d';
     }
     
     return 'gray';
@@ -67,7 +75,7 @@ function checkIsCorrectAnswer(){
 }
 
 function isValidInput(char){
-    return char >= 'a' && char <= 'z';
+    return /^[a-z]$/.test(char);
 }
 
 function onClickGridCell(e){
@@ -75,7 +83,7 @@ function onClickGridCell(e){
     // 엔터면 리셋시켜주는 코드 작성
     if(character === 'Enter' || e.keyCode === 13) {
         // 꽉 채우지 않고 엔터 누른경우
-        if(curCol !== COL) return; 
+        if(currentAttempt.length !== COL) return; 
         // 정답체크 먼저 하기
         if(checkIsCorrectAnswer()){
             return;
@@ -85,28 +93,22 @@ function onClickGridCell(e){
         attempts.push(currentAttempt);
         currentAttempt = '';
 
-        // 다음 row부터 써지게 한다. col 첫번째자리로 옮긴다.
-        curRow++;
-        curCol = 0;
         updateGrid();
         return;
     }
 
-    if(!isValidInput(character)) return;
+    if(character === 'Backspace' || e.keyCode === 8) {
+        currentAttempt = currentAttempt.slice(0, currentAttempt.length - 1);
+        updateGrid();
+        return;
+    }
+    // letter이 아니거나 6글자를 다 쓴 경우.
+    if(!isValidInput(character) || currentAttempt.length >= COL) return;
     
-    if(currentAttempt.length >= COL) return;
-        
     currentAttempt += character;
 
     return character;
 }
-document.addEventListener('keyup',(e) => {
-    const char = onClickGridCell(e);
-    if(!char || curCol === COL) return; // 엔터가 눌려야 j는 리셋됨.
-
-    updateGrid();
-    curCol++;
-});
 
 function initGrid() {
     initGlobalValue();
@@ -125,23 +127,31 @@ function initGrid() {
     }
 }
 
-// 새로 만들지 않고 children으로 접근.
 function updateGrid() {
-    for(let i = 0; i < ROW; i++){
-        const gridRow = gridWrapper.children[i];
-        for(let j = 0; j < COL; j++){
-            const gridColumn = gridRow.children[j];
-            gridColumn.textContent = '';
-            
-            if(i === curRow && currentAttempt[j]) {
-                gridColumn.textContent = currentAttempt[j];
-            }else if(i < curRow) {
-                gridColumn.textContent = attempts[i][j];
-                gridColumn.style.borderColor = getBgColor(i,j);
-            }   
-        }
+    let row = gridWrapper.firstChild; // 직접 찾지 말고 그 row 자체를 넘겨서 탐색 시킴.
+
+    for(let attempt of attempts) { 
+        // draw already recorded word (with bgColor)
+        drawAttempt(row, attempt, true);
+        row = row.nextSibling;
     }
+    // draw current attempt letters (without bgColor)
+    drawAttempt(row, currentAttempt, false);
 }
 
-initGrid();
+// 새로 만들지 않고 children으로 접근.
+function drawAttempt(row, letters, isRecorded) {
+    for(let i = 0; i < COL; i++){
+        const gridColumn = row.children[i];
+        gridColumn.textContent = letters[i] ?? '';
+        gridColumn.style.borderColor = '';
+
+        if (isRecorded) {  
+            gridColumn.style.backgroundColor = getBgColor(letters,i);
+        } else {
+            gridColumn.style.borderColor = letters[i] ? 'white' : '';
+        }
+    }
+    
+}
 

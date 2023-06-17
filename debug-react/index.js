7e9ae914571c419e682203490f8a8d13a14d9e03
gyumin_han <rbals0445@naver.com>
2023-06-18 03:19:30 +0900

    feat: add readme file

--- debug-react/index.js
+++ debug-react/index.js
@@ -1,22 +1,55 @@
-const e = React.createElement;
+debugger;
+function LikeButton() {
+  const [text, setText] = React.useState("");
 
-class LikeButton extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { liked: false };
-    }
-    render() {
-      if (this.state.liked) {
-        return 'You liked this.';
-      }
-    //   debugger
-      return e(
-        'button',
-        { onClick: () => this.setState({ liked: true }) },
-        'Like'
-      );
-    }
-  }
+  const onClick = (e, test) => {
+    console.log(e, test);
+  };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<LikeButton />)
+  const onChange = (e) => {
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
+
+  return (
+    <ul>
+      <li onClick={onClick}>Item 1</li>
+      <input type="text" onChange={onChange} value={text} />
+      <label>
+        Choose an ice cream flavor:
+        <select class="ice-cream" name="ice-cream" onChange={onChange}>
+          <option value="">Select One …</option>
+          <option value="chocolate">Chocolate</option>
+          <option value="sardine">Sardine</option>
+          <option value="vanilla">Vanilla</option>
+        </select>
+      </label>
+    </ul>
+  );
+}
+// onClick={(e) => onClick(e, 1, 2, 3, 4, 5, 6, 7)} 는 아래로 변환된다.
+
+// onClick(e) {
+//   return _onClick(e, 1, 2, 3, 4, 5, 6, 7);
+// }
+
+function LikeButtonWithoutDelegation() {
+  const onClick = (index) => () => {
+    console.log(index);
+  };
+
+  return (
+    <ul onClick={() => console.log("엣헴 버블링 성공")}>
+      {Array(15000).map((_, index) => (
+        <li onClick={onClick(index)} key={index}>
+          {index}
+        </li>
+      ))}
+    </ul>
+  );
+}
+// debugger;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<LikeButton />);
+
+console.dir(LikeButton());

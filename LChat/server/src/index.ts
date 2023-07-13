57b0583ca9fd5cbcff2634743fc05328038965a2
gyumin_han <rbals0445@naver.com>
2023-07-14 02:21:47 +0900

    feat: add prettier

--- LChat/server/src/index.ts
+++ LChat/server/src/index.ts
@@ -1,21 +1,18 @@
-import express from 'express'
-import https from 'https';
-import fs from 'fs'
+import express from "express";
+import https from "https";
+import fs from "fs";
 
 const app = express();
 const port = 9090;
 const privateKey = fs.readFileSync("key.pem");
 const certificate = fs.readFileSync("cert.pem");
-const credentials = {key: privateKey, cert: certificate};
+const credentials = { key: privateKey, cert: certificate };
 
+app.get("/", (req, res, next) => {
+  console.log("test");
+  res.send("Hello from express server");
+});
 
-app.get('/',(req,res,next) => {
-    console.log("test");
-    res.send("Hello from express server")
-})
-
-
-https.createServer(credentials,app).listen(port,()=>{
-    console.log(`[https server]: Server is listening port:${port}`);
-})
-
+https.createServer(credentials, app).listen(port, () => {
+  console.log(`[https server]: Server is listening port:${port}`);
+});
